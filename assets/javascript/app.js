712bf528b39f8c72ddd785006e2256e04fe826a4
hillary-joyce <hillary.j.scofield@gmail.com>
2018-03-29 19:25:46 -0400

    added animations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
   for(i=1; i < 16; i++){
-    $('.hex-'+i).addClass('move');
+    $('.header > div > .hex-'+i).addClass('move');
   }
+  $(".title").delay(2800).fadeIn(1300);
 });
+
+var skills = $('.skills');
+skills.waypoint(function() {
+  for(i=1; i < 14; i++){
+    $('.skills > div > .hex-'+i).addClass('move');
+  }
+}, {offset: '80%'});
+
+var about = $(".personal-info");
+about.waypoint(function() {
+  $(".personal-info > h2").fadeIn("slow");
+  console.log("Waypoints");
+}, {offset: '0%'})
